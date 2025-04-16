4e005c5f241a52f2278cc40de476e619e01f2be2
stephen james <jeratdevworks@gmail.com>
2025-04-16 12:49:22 +0100

    remove stack sections

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ShowcaseSection from "./sections/ShowcaseSection";
 import LogoSection from "./components/LogoSection";
 import FeatureCards from "./sections/FeatureCards";
 import ExperienceSection from "./sections/ExperienceSection";
-import TechStack from "./sections/TechStack";
+// import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
 import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
